00f41f73800b848aa51dacab21e39151793da629
Nicolas José Do Nascimento <nicolas.nascimento@dataside.com.br>
2026-03-19 11:57:29 -0300

    config: Adding pre-commit to project

---
+++ example.py
@@ -0,0 +1,11 @@
+
+def minha_funcao():
+    print("Hello, World!")
+
+def essa_e_outra_funcao():
+    print("Essa e outra funcao")
+
+
+#S essa_e_outra_funcao -> snake_case == var e função
+#S MinhaFuncao -> PascalCase == classes
+#S minhaFuncao -> camelCase == xxxx
