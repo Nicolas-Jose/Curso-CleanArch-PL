ca817b2ae1229a064dff011037324f41b1db9a3e
Nicolas José Do Nascimento <nicolas.nascimento@dataside.com.br>
2026-03-19 21:04:36 -0300

    feat: Implementing select and tests in UsersRepository

--- src/infra/db/repositories/users_repository.py
+++ src/infra/db/repositories/users_repository.py
@@ -17,4 +17,19 @@ class UsersRepository:
                 database.session.commit()
             except Exception as exception:
                 database.session.rollback()
-                raise exception    
+                raise exception
+
+    @classmethod
+    def select_users(cls, first_name:str) -> any:
+        with DBconnectionHandler() as database:
+            try:
+                users = (
+                    database.session
+                        .query(UsersEntity)
+                        .filter(UsersEntity.first_name == first_name)
+                        .all()
+                )
+                return users
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
--- src/infra/db/repositories/users_repository_test.py
+++ src/infra/db/repositories/users_repository_test.py
@@ -1,10 +1,36 @@
+import pytest
+from sqlalchemy import text
+from src.infra.db.settings.connection import DBconnectionHandler
 from .users_repository import UsersRepository
 
 
+db_connection_handler = DBconnectionHandler ()
+connection = db_connection_handler.get_engine().connect()
+
+@pytest.mark.skip(reason="Sensive test")
 def test_insert_user():
     mocked_first_name = 'first'
     mocked_last_name = 'last'
-    mocked_age = '51'
+    mocked_age = 51
 
     users_repository = UsersRepository()
     users_repository.insert_user(mocked_first_name, mocked_last_name, mocked_age)
+
+    sql = '''
+        SELECT * FROM users
+        WHERE first_name = '{}'
+        AND last_name = '{}'
+        AND age = {}
+    '''.format(mocked_first_name,mocked_last_name,mocked_age)
+    response = connection.execute(text(sql))
+    registry = response.fetchall()[0]
+
+    assert registry.first_name == mocked_first_name
+    assert registry.last_name == mocked_last_name
+    assert registry.age == mocked_age
+
+    connection.execute(
+    text("DELETE FROM users WHERE id = :id"),
+    {"id": registry.id}
+)
+    connection.commit()
