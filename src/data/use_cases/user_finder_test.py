2039fb2e3fe91eb390606b3c83072c3195a928ef
Nicolas José Do Nascimento <nicolas.nascimento@dataside.com.br>
2026-04-02 15:43:32 -0300

    feat: Implementing user_register

---
+++ src/data/use_cases/user_finder_test.py
@@ -0,0 +1,59 @@
+from src.infra.db.tests.users_repository import UsersRepositorySpy
+from .user_finder import UserFinder
+
+
+def test_find():
+    first_name = 'meunome'
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    response = user_finder.find(first_name)
+   
+    assert repo.select_user_attributes["first_name"] == first_name
+
+    assert response["type"] == 'Users'
+    assert response["count"] == len(response['attributes'])
+    assert response["attributes"] 
+
+def test_find_error_in_valid_name():
+    first_name = 'meunome123'
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as exception:
+        assert str(exception) == "Invalid name for search"
+
+
+def test_find_error_in_long_name():
+    first_name = 'meunomeasasasasasasasasasasasasasasasasasas'
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as exception:
+        assert str(exception) == "Name too long for search"
+
+
+def test_find_error_user_not_found():
+    class UsersRepositoryError(UsersRepositorySpy):
+        def select_user(self, first_name: str):
+            return []
+
+    first_name = 'meunome'
+
+    repo = UsersRepositoryError()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as exception:
+        assert str(exception) == "User not found"
